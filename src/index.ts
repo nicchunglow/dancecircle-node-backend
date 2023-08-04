a753396aea8d81cbb5233ac00c8f8bb19a209252
nicchunglow <iamnicholaschung@gmail.com>
2023-08-04 14:25:33 +0800

    Update code to use sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import "dotenv/config";
-import connectDB from "./utils/db";
+// index.ts
+import "dotenv";
+import sequelize from "./utils/db";
 import app from "./app";
 
 const PORT = process.env.PORT || 3000 || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Started Express app on http://localhost:${PORT}`);
+sequelize.authenticate().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+  });
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
 });
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,16 +1,6 @@
 import "dotenv";
 import { Sequelize } from "sequelize";
 
-const dbName = "CanILoginDB";
+const sequelize = new Sequelize(`postgres://test-user:wrong@localhost:5432/testdb`);
 
-const sequelize = new Sequelize(
-  `${process.env.DB_SCHEMA_NAME}`,
-  `${process.env.DB_USERNAME}`,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-  }
-);
-
-export default { Sequelize, sequelize };
+export default sequelize;
