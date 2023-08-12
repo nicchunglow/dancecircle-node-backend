251a97d7038e062ca90b3829374947894449b4a6
nicchunglow <iamnicholaschung@gmail.com>
2023-08-12 17:27:52 +0800

    Update code to mock router

--- src/__tests__/routes/userRouter.test.ts
+++ src/__tests__/routes/userRouter.test.ts
@@ -8,10 +8,15 @@ app.use("/", usersRouter); // Mount the router in the Express app
 
 describe("Users Route Test", () => {
   it("should respond with a list of users", async () => {
+    const mockUsers = [
+      { id: 1, firstName: "John", lastName: "Doe", email: "john@example.com" },
+      { id: 2, firstName: "Jane", lastName: "Doe", email: "jane@example.com" },
+    ];
+    jest.spyOn(UserController, "getUsers").mockResolvedValue(mockUsers as any);
     const response = await request(app).get("/");
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual(expect.arrayContaining([])); // Update with your expected response format
+    expect(response.body).toEqual(mockUsers);
   });
 
   it("should handle errors gracefully", async () => {
